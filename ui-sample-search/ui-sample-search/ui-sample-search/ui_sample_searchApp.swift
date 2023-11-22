49cc848049d205284f3a621912c0dab4248d82d4
Florencia <floojeda99@gmail.com>
2023-11-22 09:33:12 -0300

    UI first iteration

---
+++ ui-sample-search/ui-sample-search/ui-sample-search/ui_sample_searchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ui_sample_searchApp.swift
+//  ui-sample-search
+//
+//  Created by Flor on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ui_sample_searchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
